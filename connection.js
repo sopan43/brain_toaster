7412ebbec36906b7d4d5fd58da6d029c33feeaeb
Sopan Mittal <sopanmittal43@gmail.com>
2018-02-19 00:10:15 +0530

    database check

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'bedbc9cc7d11fa',
-    password: '0a93b38cd911cf',
+    password: '0a93b38cd911cfa',
     database: 'heroku_1141cf6ca7b533b'
     //user : 'root', 
     //  password : 'root123',  
